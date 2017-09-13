4d155281d5a61839c897274be930b7c7f338108d
Connor Bray <connor.bray@andoverlab.co.uk>
2017-09-13 15:03:44 +0100

    Working lambda to create origin access identities

--- lambdas/cloudfront-origin-access-identity/lambda_function.py
+++ lambdas/cloudfront-origin-access-identity/lambda_function.py
@@ -1,3 +1,4 @@
+import sys
 import json
 import boto3
 import cfnresponse
@@ -8,8 +9,8 @@ client = boto3.client('cloudfront')
 def create_identity(event):
     response = client.create_cloud_front_origin_access_identity(
         CloudFrontOriginAccessIdentityConfig={
-            'CallerReference': event['Reference'],
-            'Comment': event['Comment']
+            'CallerReference': event['ResourceProperties']['Reference'],
+            'Comment': event['ResourceProperties']['Comment']
         }
     )
     return response
@@ -18,58 +19,67 @@ def lambda_handler(event, context):
     print("Received event: " + json.dumps(event, indent=2))
     success = True
     reason = None
-    responseData = {}
+    responseData = {
+        'data': {
+            'CallerReference': event['ResourceProperties']['Reference'],
+            'Comment': event['ResourceProperties']['Comment']
+        }
+    }
 
-    if event['RequestType'] == 'Create':
-        try:
-            response = create_identity(event)
-            if response['CloudFrontOriginAccessIdentity'] is not None:
-                success = True
-                responseData['data'] = {
-                    'id': response['CloudFrontOriginAccessIdentity']['Id'],
-                    'S3CanonicalUserId': response['CloudFrontOriginAccessIdentity']['S3CanonicalUserId']
-                }
-            else:
-                success = False
-                reason = 'CREATE operation was not successful, no Origin Access Identity info in response'
-        except:
-            success = False
-            reason = 'Origin Access Identity already exists for Reference ' + event['Reference']
-    else:
-        listIdentitiesResponse = client.list_cloud_front_origin_access_identities(
-            MaxItems='999'
-        )
-        for item in listIdentitiesResponse['CloudFrontOriginAccessIdentityList']['Items']:
-            identityInfo = client.get_cloud_front_origin_access_identity_config(
-                Id=item['Id']
-            )
-            if identityInfo['CloudFrontOriginAccessIdentityConfig']['CallerReference'] == event['Reference']:
-                if event['RequestType'] == 'Delete':
-                    client.delete_cloud_front_origin_access_identity(
-                        Id=item['Id'],
-                        IfMatch=identityInfo['ETag']
-                    )
+    try:
+        if event['RequestType'] == 'Create':
+            try:
+                response = create_identity(event)
+                print('Response: {}'.format(response))
+                if response['CloudFrontOriginAccessIdentity'] is not None:
                     success = True
+                    responseData['data']['Id'] = response['CloudFrontOriginAccessIdentity']['Id']
+                    responseData['data']['S3CanonicalUserId'] = response['CloudFrontOriginAccessIdentity']['S3CanonicalUserId']
                 else:
-                    updateResponse = client.update_cloud_front_origin_access_identity(
-                        CloudFrontOriginAccessIdentityConfig={
-                            'CallerReference': event['Reference'],
-                            'Comment': event['Comment']
-                        },
-                        Id=item['Id'],
-                        IfMatch=identityInfo['ETag']
-                    )
-                    if updateResponse['CloudFrontOriginAccessIdentity'] is not None:
+                    success = False
+                    print(sys.exc_info()[0])
+                    reason = 'CREATE operation was not successful, no Origin Access Identity info in response'
+            except:
+                success = False
+                print(sys.exc_info()[0])
+                reason = 'Origin Access Identity already exists for Reference ' + event['ResourceProperties']['Reference']
+        else:
+            listIdentitiesResponse = client.list_cloud_front_origin_access_identities(
+                MaxItems='999'
+            )
+            for item in listIdentitiesResponse['CloudFrontOriginAccessIdentityList']['Items']:
+                identityInfo = client.get_cloud_front_origin_access_identity_config(
+                    Id=item['Id']
+                )
+                if identityInfo['CloudFrontOriginAccessIdentityConfig']['CallerReference'] == event['ResourceProperties']['Reference']:
+                    if event['RequestType'] == 'Delete':
+                        client.delete_cloud_front_origin_access_identity(
+                            Id=item['Id'],
+                            IfMatch=identityInfo['ETag']
+                        )
                         success = True
-                        responseData['data'] = {
-                            'id': updateResponse['CloudFrontOriginAccessIdentity']['Id'],
-                            'S3CanonicalUserId': updateResponse['CloudFrontOriginAccessIdentity']['S3CanonicalUserId']
-                        }
                     else:
-                        success = False
-                        reason = 'Failed to UPDATE Origin Access Identity'
-                break
-    cfnresponse.send(event, context, cfnresponse.SUCCESS if success else cfnresponse.FAILED, responseData, 'origin-access-identity-' + event['ResourceProperties']['StackName'])
+                        updateResponse = client.update_cloud_front_origin_access_identity(
+                            CloudFrontOriginAccessIdentityConfig={
+                                'CallerReference': event['ResourceProperties']['Reference'],
+                                'Comment': event['ResourceProperties']['Comment']
+                            },
+                            Id=item['Id'],
+                            IfMatch=identityInfo['ETag']
+                        )
+                        if updateResponse['CloudFrontOriginAccessIdentity'] is not None:
+                            success = True
+                            responseData['data']['Id'] = updateResponse['CloudFrontOriginAccessIdentity']['Id']
+                            responseData['data']['S3CanonicalUserId'] = updateResponse['CloudFrontOriginAccessIdentity']['S3CanonicalUserId']
+                        else:
+                            success = False
+                            reason = 'Failed to UPDATE Origin Access Identity'
+                    break
+    except:
+        success = False
+        print(sys.exc_info()[0])
+        reason = 'Unexpected exception occurred'
+    cfnresponse.send(event, context, cfnresponse.SUCCESS if success else cfnresponse.FAILED, reason, responseData, 'origin-access-identity-' + event['ResourceProperties']['Reference'])
     print('Success: {}'.format(success))
     print('Reason: {}'.format(reason))
     print('Response: {}'.format(responseData))
